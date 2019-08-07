ca0fcfa3859570f52515f860efb587a511b1d708
William Nguyen <williamnguyen@Williams-MacBook-Pro.local>
2019-08-06 19:33:25 -0700

    basically the final commit

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 /* NAVBAR */
 
-//Navbar Color Fade
+//Navbar Color Fade Upon Scroll
 $(function () {
     $(document).scroll(function () {
         var $nav = $("#mainNavbar");
         $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
     });
 });
-
-// //Navbar Active Tab State
-// $('#mainNavbar .navbar-nav a').on('click', function () {
-//     $('#mainNavbar .navbar-nav').find('li.active').removeClass('active');
-//     $(this).parent('li').addClass('active');
-// });
